00f8bc9568fe289348796024f2e35cfac1f50352
Dor Bezalel <dor2081@gmail.com>
2023-05-10 12:20:35 +0300

    add Home page to view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartDisplayComponent } from './components/chart-display/chart-display.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { NodesDisplayComponent } from './components/nodes-display/nodes-display.component';
+import { D3NetworkGraphComponent } from './components/d3-network-graph/d3-network-graph.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartDisplayComponent
+    ChartDisplayComponent,
+    NodesDisplayComponent,
+    D3NetworkGraphComponent,
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
