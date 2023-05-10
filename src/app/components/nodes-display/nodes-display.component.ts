a37ffa2d74918f604edbd6b4bcd35a89f996fcbd
Dor Bezalel <dor2081@gmail.com>
2023-05-10 12:11:25 +0300

    create NodesDisplayComponent for highcharts costume node display

---
+++ src/app/components/nodes-display/nodes-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nodes-display',
+  templateUrl: './nodes-display.component.html',
+  styleUrls: ['./nodes-display.component.scss']
+})
+export class NodesDisplayComponent {
+
+}
