4c7ed4aedeb0e4bc6cb4dfac8d96877b84f576be
Dor Bezalel <dor2081@gmail.com>
2023-05-10 12:09:52 +0300

    add views with router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomePageComponent} from "./views/home-page/home-page.component";
+import {ChartDisplayComponent} from "./components/chart-display/chart-display.component";
+import {D3NetworkGraphComponent} from "./components/d3-network-graph/d3-network-graph.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'highcharts', component: ChartDisplayComponent},
+  {path: 'D3', component: D3NetworkGraphComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
