56da70d49f6bfeabd5cedf1c949e6b92e4816967
Dor Bezalel <dor2081@gmail.com>
2023-05-10 12:21:29 +0300

    add d3.js

---
+++ src/app/components/d3-network-graph/d3-network-graph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-d3-network-graph',
+  templateUrl: './d3-network-graph.component.html',
+  styleUrls: ['./d3-network-graph.component.scss']
+})
+export class D3NetworkGraphComponent {
+
+}
