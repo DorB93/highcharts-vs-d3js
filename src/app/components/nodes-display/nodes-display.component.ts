c69ae8e6bb8a2868b1e123a5dbaba8a0c28cbc3f
Dor Bezalel <dor.bezalel@vulcan.io>
2023-05-11 11:57:43 +0300

    NodesDisplayComponent receive an Input

--- src/app/components/nodes-display/nodes-display.component.ts
+++ src/app/components/nodes-display/nodes-display.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-nodes-display',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./nodes-display.component.scss']
 })
 export class NodesDisplayComponent {
-
+  @Input() name: string = '';
 }
